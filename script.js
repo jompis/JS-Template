a7b7b75dc3939ed75646134031877e1730a44e40
jompis <jonatan.vangborg@skola.taby.se>
2022-01-28 11:15:59 +0100

    commit

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const myString = "Hello, world!";
-console.log(myString);
+let pengar = number(prompt("Hur mycket pengar vill du spendera?: "))
+let järnsmycke = 200
+let silversmycke = 500
+let guldsmycke = 800
+
+if (pengar < järnsmycke){
+    console.log("Du har tyvärr inte råd med något smycke!")
+}
+if (pengar > järnsmycke, pengar < 2*järnsmycke){
+    console.log("Du har råd med två järnsmycken")
+}
